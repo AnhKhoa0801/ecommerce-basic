03f09d265c8f4e3d40b9c97c4f71bfdea39b8be3
AnhKhoa0801 <vomiller882@gmail.com>
2022-01-14 16:12:26 +0700

    api check data

--- pages/api/order.js
+++ pages/api/order.js
@@ -1,16 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { saveOrder } from "dataService/orderService";
+import { getOrders } from "dataService/orderService";
 
-export default function handler(req, res) {
-  if (req.method !== "POST") {
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const order = req.body;
+      saveOrder(order);
+      res.status(200).json("order success!");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else if (req.method === "GET") {
+    const orders = await getOrders();
+    res.status(200).json(orders);
+  } else {
     res.status(404).json({ message: "not support method" });
-  }
-
-  try {
-    const order = req.body;
-    saveOrder(order);
-    res.status(200).json("order success!");
-  } catch (error) {
-    res.status(500).json(error);
   }
 }
